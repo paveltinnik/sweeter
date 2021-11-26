ae92872486ae8dfa51618fb9a0146d181d7a52d3
Pavel Tinnikov <t280396ab@mail.ru>
2021-11-26 21:59:43 +0700

    changed hostname

--- src/main/java/com/paveltinnik/service/UserService.java
+++ src/main/java/com/paveltinnik/service/UserService.java
@@ -61,7 +61,7 @@ public class UserService implements UserDetailsService {
         if (!StringUtils.isEmpty(user.getEmail())) {
             String message = String.format(
                     "Hello, %s! \n" +
-                            "Welcome to Sweater. Please, visit next link: http://%s/activate/%s",
+                            "Welcome to Sweeter. Please, visit next link: http://%s/activate/%s",
                     user.getUsername(),
                     hostname,
                     user.getActivationCode()
